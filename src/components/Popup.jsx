2ab7032d09d06856c9b24b65f0d4e3500605b6c6
ArtyomYaprintsev <yapryntsev.a02@mail.ru>
2023-09-28 21:50:09 +0500

    Add scss layouts

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-// import "../assets/scss/popup.scss";
-
 const Popup = ({ closePopup, popupClassName = "", body = <></> }) => {
   return (
     <div className='popup-wrapper'>
       <div className='close-popup-wrapper' onClick={closePopup}></div>
+
       <div className={`popup-container container ${popupClassName}`}>
-        {body && <div className='popup-body'>{body}</div>}
+        {body}
         <button className='close-popup' onClick={closePopup}>
           Close
         </button>
--- src/components/ResultPopup.jsx
+++ src/components/ResultPopup.jsx
@@ -7,6 +7,7 @@ const ResultPopup = ({
   onClose = () => {},
 }) => {
   const [isOpened, setIsOpened] = useState(false);
+  const [isCopied, setIsCopied] = useState(false);
 
   useEffect(() => {
     if (resultValue) setIsOpened(true);
@@ -17,6 +18,29 @@ const ResultPopup = ({
       onClose();
     }
   }, [isOpened]);
+
+  useEffect(() => {
+    let timeoutId;
+
+    if (isCopied) {
+      timeoutId = setTimeout(() => {
+        setIsCopied(false);
+      }, 2000);
+    }
+
+    return () => clearTimeout(timeoutId);
+  }, [isCopied]);
+
+  const copyResult = (text) => {
+    if (!navigator.clipboard) {
+      return;
+    }
+
+    navigator.clipboard
+      .writeText(text)
+      .then(() => setIsCopied(true))
+      .catch((err) => console.error("Async: Could not copy text: ", err));
+  };
 
   if (!isOpened) {
     return <></>;
@@ -29,7 +53,22 @@ const ResultPopup = ({
       body={
         <>
           <div className='action-header'>Action header</div>
-          <div className='result__wrapper'>{resultValue}</div>
+          <div className='result__wrapper'>
+            <div className='result__header'>
+              <span>Result</span>
+              {resultValue && navigator.clipboard && (
+                <button
+                  className={`copy-btn${isCopied ? " copied" : ""}`}
+                  type='button'
+                  onClick={() => copyResult(resultValue)}
+                >
+                  Copy
+                </button>
+              )}
+            </div>
+
+            {resultValue}
+          </div>
         </>
       }
     />
