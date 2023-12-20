aa25afc0c83436f7367867f9263747c920f94e62
ArtyomYaprintsev <yapryntsev.a02@mail.ru>
2023-12-20 06:28:16 +0500

    Fix bug with ru-only cipher validation

--- src/components/CipherInputWrapper.jsx
+++ src/components/CipherInputWrapper.jsx
@@ -12,18 +12,21 @@ const CipherInputWrapper = ({ register, errors, isRuOnly }) => {
         id='cipher-text'
         {...register("cipherText", {
           required: "The cipher text is required.",
-          pattern: {
-            value: !isRuOnly
-              ? /^(?:(?:[A-Za-zА-Яа-яЁё]{5})[-\s])*[A-Za-zА-Яа-яЁё]{1,5}\s?$/gm
-              : /^(?:(?:[А-Яа-яЁё]{5})[-\s])*[А-Яа-яЁё]{1,5}\s?$/gm,
-            message: "Invalid cipher value, check hints.",
+          validate: {
+            checkAlphabet: (value) => {
+              const isContainLatinCharacters = /[a-z]/i.test(value);
+
+              if (isRuOnly && isContainLatinCharacters) {
+                return "Cipher can not to contain latin characters";
+              }
+            },
           },
         })}
       />
 
       <HintList
         hints={[
-          "Cipher text must be in groups of 5 characters (excluding the last group), separated by a '-' sign or a space.",
+          "Cipher text must be in groups of n characters (excluding the last group), separated by a '-' sign or a space.",
           `Cipher can only contains letters of the ${
             isRuOnly ? "Russian alphabet" : "Russian and Latin alphabets"
           }.`,
